49f93d44b3144224cdbd0938104c929422298479
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-06 13:48:39 +0300

    "Initial commit"

---
+++ src/test/java/com/example/demouserverification/DemoUserVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demouserverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
